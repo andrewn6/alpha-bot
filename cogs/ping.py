ebab18caba6f90eafbd3896bfc850638044145b5
andrewnijmeh <andrewnijmeh1@gmail.com>
2020-11-06 18:32:55 -0500

    Create ping command for latency

---
+++ cogs/ping.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"Pong {self.client.latency}"")
+
+
+def setup(client):
+    client.add_cog(Ping(client))
