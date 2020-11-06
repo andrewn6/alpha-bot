e42909670334e15204cffbebcde2d83c6b36fbf5
andrew <andrewnijmeh1@gmail.com>
2020-11-06 18:28:28 -0500

    formatting

--- alpha-bot-dev/cogs/ping.py
+++ alpha-bot-dev/cogs/ping.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 class Ping(commands.Cog):
     def __init__(self, client):
         self.client = client
+        
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f"Pong {self.client.latency}"")
