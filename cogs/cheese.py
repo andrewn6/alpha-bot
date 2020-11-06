31b4ef4c3c5c365c05a9c2cfe3f31c382464e32a
Pyroseza <jarpri08@gmail.com>
2020-11-06 00:18:32 +0000

    added reaction tracker to collect cheese

--- cogs/cheese.py
+++ cogs/cheese.py
@@ -1,10 +1,11 @@
 """This is a cog for a discord.py bot.
 It drops random cheese for people to pick up
 """
-import random
+from collections import defaultdict
 from datetime import datetime as dt
-from discord.ext import commands
 from discord import Activity, DMChannel
+from discord.ext import commands
+import random
 
 
 class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
@@ -13,7 +14,7 @@ class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
         self.last_cheese = dt.utcnow()
         self.cheese_weight = (100 - self.client.config.get("cheese_weight", 50), 100)
         self.cooldown = 30
-        self.msg_memory = dict()
+        self.scores = defaultdict(int)
         random.seed()
 
     @commands.Cog.listener()
@@ -23,18 +24,25 @@ class Cheese(commands.Cog, command_attrs=dict(hidden=True)):
         if isinstance(msg.channel, DMChannel):
             # Ignore DM
             return
-
+        client = self.client
         chance_result = random.choices([0,1], cum_weights=self.cheese_weight)[0]
-        self.client.log.debug(f"{chance_result=}")
+        client.log.debug(f"{chance_result=}")
         if chance_result:
             #if (dt.utcnow() - self.last_cheese).total_seconds() < self.cooldown:
             #    return
-            if (msg_id := msg.id) in self.msg_memory.keys():
-                return
-            self.msg_memory[msg_id] = ""
-            await msg.add_reaction('🧀')
             message = 'A wild cheese appeared!'
             await msg.channel.send(message)
+            await msg.add_reaction('🧀')
+            def check(reaction, user):
+                return not user.bot and str(reaction.emoji) == '🧀'
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+                self.scores[user.id] += 1
+            except asyncio.TimeoutError:
+                await msg.channel.send('👎')
+            else:
+                await msg.channel.send('👍')
+                await msg.channel.send(f"Cheeses collected: {self.scores}")
             self.last_cheese = dt.utcnow()
 
 
